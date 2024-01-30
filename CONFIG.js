72e793b09ecf3e87a740e0a97b74d308416c5ad9
elORE0 <158122296+elORE0@users.noreply.github.com>
2024-01-29 23:14:23 -0500

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "QUIERES SER MI SAN VALENTIN",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "TE AMO PRINCESA ESTE SERA NUESTRO SEGUNDO 14 DE FEBRERO JUNTOS",
+  buttonYes: "SI, ACEPTO",
+  buttonNo: "NO,GRACIAS",
+  titleModar: "PARA LA MAS LINDA",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Este año esta lleno de cambios y tendremos que adaptarnos, debido a esto tenemos mirar como van a funcionar nuestras celebraciones </br> pero esto no va a impedir demostrarte el amor que siento por tu</br> Lov you <3",
 };
